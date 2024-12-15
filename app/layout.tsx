2c1f5d7f20474fdeb5c098ad9e27700f5f212328
izumin-baal <milton96.engineer@gmail.com>
2024-12-15 18:58:22 +0000

    study1の作成中

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@material-tailwind/react";
 import "./globals.css";
 
 const geistSans = Geist({
--- app/study1/page.tsx
+++ app/study1/page.tsx
@@ -1,13 +1,24 @@
 'use client';
-import React from "react";
+import React, { useState } from "react";
 import Container from "@/components/layout/container";
+import { Button } from "@material-tailwind/react";
 
 const Page = () => {
-  return (
-    <Container>
-        <h4>useReducerの使い方</h4>
-    </Container>
-  );
+    const [state, setState] = useState(0);
+
+    const countUp = () => {
+        setState(prev => ++prev)
+    }
+
+    return (
+        <Container>
+            <h4>useReducerの使い方</h4>
+            useEffectと違ってアクションも同時に定義する。
+            <h5>useEffectの例</h5>
+            <p className="mb-4 font-bold">{state}</p>
+            <Button onClick={countUp}>+</Button>
+        </Container>
+    );
 }
 
 export default Page;
